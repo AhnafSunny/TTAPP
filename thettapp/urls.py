389ca791669f9454dfa66593c220aa9ce2eeca8b
AhnafSunny <ahnafx88@gmail.com>
2017-01-25 17:54:49 +0600

    added views function for all create

--- thettapp/urls.py
+++ thettapp/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import url,include
 from . import views
 
+
+app_name = 'thettapp'
+
 urlpatterns = [
-    url(r'^$',views.index),
+    url(r'^$',views.index, name= 'index'),
+    url(r'^player/create',views.playerCreate,name='playerCreate'),
+    url(r'^team/create',views.teamCreate,name='teamCreate'),
+    url(r'^match/create',views.matchCreate,name='matchCreate'),
 ]
--- thettapp/views.py
+++ thettapp/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render,render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 
 def index(request):
 
     return render(request,'thettapp/index.html')
+
+def playerCreate(request):
+    return HttpResponse('<h1>player create</h1>')
+
+def teamCreate(request):
+    return HttpResponse('<h1>team create</h1>')
+
+def matchCreate(request):
+    return HttpResponse('<h1>match create</h1>')
